0f5b770f0bd1eb639e26f0cab6360ee6bd7469a6
TUJJIEVE <cs16btech11039@iith.ac.in>
2020-05-04 12:49:10 +0530

    add editing answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -7,6 +7,21 @@ class AnswersController < ApplicationController
         redirect_to question_path(@question)
     end
 
+    def edit
+        @question = Question.find(params[:question_id])
+        @answer = @question.answers.find(params[:id])        
+    end
+
+    def update
+        @question = Question.find(params[:question_id])
+        @answer = @question.answers.find(params[:id])
+        if @answer.update(params.require(:answer).permit(:posted_by,:body))
+            redirect_to @question
+        else
+            render 'edit'
+        end
+    end
+
     def destroy
         @question = Question.find(params[:question_id])
         @answer = @question.answers.find(params[:id])
