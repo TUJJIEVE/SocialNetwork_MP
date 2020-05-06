fa3eb4f11b83f8aa5efbf6d5d5c1bac359cfebed
rajesh kumar <beginedin199@gmail.com>
2020-05-06 23:41:16 +0530

    added pages#index,misc integration tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
@@ -9,5 +10,29 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  include Devise::Test::IntegrationHelpers
+  include Warden::Test::Helpers
+
   # Add more helper methods to be used by all tests here...
+
+  def log_in (user)
+    if integration_test?
+      login_as(user,:scope=>:user)
+    else
+      sign_in(user)
+    end
+  end
+
+  def log_out (user)
+    if integration_test?
+      logout(user,:scope=>:user)
+    else
+      sign_out(user)
+    end
+  end
+  
+  def integration_test?
+    defined?(post_via_redirect)
+  end
+ 
 end
