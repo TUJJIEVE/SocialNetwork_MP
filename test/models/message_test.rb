9785dd38ede7790f7e398147e4e721b7bf8b3833
rajesh kumar <beginedin199@gmail.com>
2020-05-06 22:25:19 +0530

    added conv,msg Model unit tests

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup    
+    @users = [users(:user_one) ,users(:user_two) , users(:user_three)] 
+    [0,1,2].each{ |i|
+      @users[i].save
+    }
+    @conversations = [conversations(:one),conversations(:two)]
+    [0,1].each{ |i|
+      @conversations[i].save
+    }
+    @messages = [messages(:one),messages(:two),messages(:three),messages(:four)]
+    [0,1,2,3].each{ |i|
+      @messages[i].save
+    }
+  end
+  
+  test "allAreInternal" do 
+    assert true
+  end
+  
 end
