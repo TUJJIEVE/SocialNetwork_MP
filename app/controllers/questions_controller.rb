917e930e22f4b220b22a185046964beffde486b4
rajesh kumar <beginedin199@gmail.com>
2020-05-08 08:19:57 +0530

    minor fix on views

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,10 +6,19 @@ class QuestionsController < ApplicationController
 
 
     def new
-        @question = Question.new
+        if user_signed_in?  
+            @question = Question.new
+        else
+            flash[:notice] = "Notice :: please register to ask a question"
+            redirect_to root_path
+        end
     end
     def edit
-        @question = Question.find(params[:id])
+        @question = params[:id].present? ? Question.find_by_id(params[:id]) : nil     
+        if @question == nil 
+            flash[:error] = "question not found"
+            redirect_to root_path
+        end  
     end
     def destroy
         @question = Question.find(params[:id])
@@ -18,69 +27,94 @@ class QuestionsController < ApplicationController
         redirect_to questions_path
     end
 
-    def upvote
-        @question = Question.find(params[:id])
-        @is_ok = QVote.check_if_present(current_user.uname,params[:id])
-        
-        if @is_ok != nil && @is_ok[0].vote_type !=1
-            puts "Already voted this question"
-        else
-            if ((@is_ok != nil) && (@is_ok[0].vote_type == 1))
-                puts "Already downvoted this question destroying"
-                @is_ok[0].destroy
-            end 
-            puts "Upvoting this question"
-            @upvote = @question.q_votes.build()
-            @upvote.vote_type = 0
-            @upvote.voted_by = current_user.uname
-            puts @upvote.vote_id
-            if @upvote.save
-                redirect_to questions_path(@question)
+    def upvote    
+        @question = params[:id].present? ? Question.find_by_id(params[:id]) : nil     
+        if @question == nil 
+            flash[:error] = "question not found"
+            redirect_to root_path
+        end   
+        if user_signed_in?           
+            @is_ok = QVote.check_if_present(current_user.uname,params[:id])
+            
+            if @is_ok != nil && @is_ok[0].vote_type !=1
+                puts "Already voted this question"
             else
-                puts "ERRROR :cannot vote"
+                if ((@is_ok != nil) && (@is_ok[0].vote_type == 1))
+                    flash[:notice] = "Already downvoted this question"
+                    @is_ok[0].destroy
+                end 
+                puts "Upvoting this question"
+                @upvote = @question.q_votes.build()
+                @upvote.vote_type = 0
+                @upvote.voted_by = current_user.uname
+                puts @upvote.vote_id
+                if @upvote.save
+                    flash[:success] = "Success: vote is registered"
+                else
+                    flash[:error] = "ERRROR : vote is not saved "
+                end
+                redirect_to questions_path(@question)
             end
+        else
+            flash[:notice] = "please register to vote"
+            redirect_to questions_path(@question)
         end
     end
     def downvote
-        @question = Question.find(params[:id])
-
-
-
-        @is_ok = QVote.check_if_present(current_user.uname,params[:id])
-        if @is_ok != nil && @is_ok[0].vote_type !=0
-
-            puts "ALready Voted this question"
-        
-        else
-            if ( (@is_ok != nil) && (@is_ok[0].vote_type == 0))
-                puts @is_ok[0].id
-                @is_ok[0].destroy
-                puts "Already upvoted this question now destroying"
-            end            
-            puts "Downvoting this question"
-            @downvote = @question.q_votes.build()
-
-            @downvote.vote_type = 1
-            @downvote.voted_by = current_user.uname
-            puts @downvote.vote_id
-            if @downvote.save
-                redirect_to question_path(@question)
+        @question = params[:id].present? ? Question.find_by_id(params[:id]) : nil     
+        if @question == nil 
+            flash[:error] = "question not found"
+            redirect_to root_path
+        end   
+        if user_signed_in?  
+            @is_ok = QVote.check_if_present(current_user.uname,params[:id])
+            if @is_ok != nil && @is_ok[0].vote_type !=0
+                flash[:notice] = "Already voted this question"
+                puts "ALready Voted this question"
+            
             else
-                puts "Error :cannot vote"
+                if ( (@is_ok != nil) && (@is_ok[0].vote_type == 0))
+                    puts @is_ok[0].id
+                    @is_ok[0].destroy
+                    puts "Already upvoted this question now destroying"
+                end            
+                puts "Downvoting this question"
+                @downvote = @question.q_votes.build()
+
+                @downvote.vote_type = 1
+                @downvote.voted_by = current_user.uname
+                puts @downvote.vote_id
+                if @downvote.save
+                    redirect_to question_path(@question)
+                else
+                    puts "Error :cannot vote"
+                end
             end
+        else
+            flash[:notice] = "please register to vote"
+            redirect_to questions_path(@question)
         end
     end
-    def update
-        @question = Question.find(params[:id])
-        # date = params[:date]
-        # time = params[:time]
-        edited_at = DateTime.now
-        @question.updated_at = edited_at
-        # puts edited_at  
-        if @question.update(params.require(:question).permit(:title,:text))
-            redirect_to @question
-        else 
-            render 'edit'
+    def update        
+        @question = params[:id].present? ? Question.find_by_id(params[:id]) : nil     
+        if @question == nil 
+            flash[:error] = "question not found"
+            redirect_to root_path
+        end  
+        if user_signed_in?  
+            # date = params[:date]
+            # time = params[:time]
+            edited_at = DateTime.now
+            @question.updated_at = edited_at
+            # puts edited_at  
+            if @question.update(params.require(:question).permit(:title,:text))
+                redirect_to @question
+            else 
+                render 'edit'
+            end
+        else
+            flash[:notice] = "please register to update"
+            redirect_to questions_path(@question)
         end
     end
 
